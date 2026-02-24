803f01d1c3b671c16f8c962f2cd44f2896ca51cc
Rajesh K <rajesh3655r@gmail.com>
2026-02-24 22:27:36 +0530

    Optimize homepage render path with compiled Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './scripts/jobs-feed.js',
+    './scripts/site.js'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
+
